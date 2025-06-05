e03800d38c6d250db72155ca58958f71231ad1e2
okan <okanvurdu@gmail.com>
2025-06-05 20:20:19 +0300

    test: rollback integration_test.go file to as-is version

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -16,12 +16,7 @@ type AirlineEvent struct {
 	name string
 }
 
-func TestDefaultMapper(t *testing.T) {
-	t.Run("TestDefaultMapperInsert", testDefaultMapperInsert)
-	t.Run("TestDefaultMapperDelete", testDefaultMapperDelete)
-}
-
-func testDefaultMapperInsert(t *testing.T) {
+func TestSql(t *testing.T) {
 	time.Sleep(time.Second * 30)
 
 	connector, err := dcpsql.NewConnectorBuilder("config.yml").Build()
@@ -88,77 +83,6 @@ func testDefaultMapperInsert(t *testing.T) {
 			}
 		}
 
-	}()
-
-	wg.Wait()
-}
-
-func testDefaultMapperDelete(t *testing.T) {
-	time.Sleep(time.Second * 30)
-
-	connector, err := dcpsql.NewConnectorBuilder("config.yml").Build()
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		connector.Start()
-	}()
-
-	time.Sleep(1 * time.Second)
-
-	go func() {
-		sql, err := client.NewSQLClient(config.SQL{
-			Host:       "localhost",
-			User:       "user",
-			Password:   "password",
-			DBName:     "example",
-			Port:       5432,
-			DriverName: "postgres",
-			SslMode:    "disable",
-			CollectionTableMapping: []config.CollectionTableMapping{
-				{
-					Collection:      "_default",
-					TableName:       "public.example_table",
-					KeyColumnName:   "id",
-					ValueColumnName: "name",
-					Audit: struct {
-						Enabled             bool   `yaml:"enabled"`
-						CreatedAtColumnName string `yaml:"createdAtColumnName"`
-						UpdatedAtColumnName string `yaml:"updatedAtColumnName"`
-					}{},
-				},
-			},
-		})
-		if err != nil {
-			t.Fatalf("could not open connection to sql %s", err)
-		}
-
-		ctx, _ := context.WithTimeout(context.Background(), 3*time.Minute)
-
-	DeleteCheckLoop:
-		for {
-			select {
-			case <-ctx.Done():
-				t.Fatalf("deadline exceed")
-			default:
-				var count int
-				err := sql.QueryRow("SELECT COUNT(*) FROM example_table").Scan(&count)
-				if err != nil {
-					t.Fatalf("sql query error %s", err)
-				}
-				if count == 0 {
-					logger.Log.Info("all records deleted successfully")
-					connector.Close()
-					goto DeleteCheckLoop
-				}
-				time.Sleep(2 * time.Second)
-			}
-		}
 	}()
 
 	wg.Wait()
